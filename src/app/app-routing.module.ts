11fcc13f3d945807abfe75fa3a0c7ec24eb8d4c3
unknown <clz@clz.me>
2019-07-18 10:39:42 +0800

    取消auth路由守卫

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) },
-  { path: '', loadChildren: () => import('./core/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
+  { path: '', loadChildren: () => import('./core/dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
 @NgModule({
