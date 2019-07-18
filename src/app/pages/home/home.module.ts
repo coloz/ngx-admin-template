d85fab816fd8c84fcd19080806616287db066214
unknown <clz@clz.me>
2019-07-19 01:55:17 +0800

    更新

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home.component';
 import { CoreComponentsModule } from 'src/app/core/components/core-components.module';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		NgbModule,
 		CoreComponentsModule,
+		MarkdownModule.forRoot(),
 		RouterModule.forChild([
 			{
 				path: '',
